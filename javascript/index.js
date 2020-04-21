3c3d9fe153d7d6bdae8e042d9ca88a6e505c7cfe
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-04-21 15:30:45 +1000

    Check-in js version

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+const {createObjectCsvWriter} = require('csv-writer')
+
+const params = {
+  path: '__languages.csv',
+  header: [
+    {id: 'name', title: 'NAME'},
+    {id: 'lang', title: 'LANGUAGE'}
+  ]
+}
+const records = [
+  {name: 'Bob', lang: 'French, English'},
+  {name: 'Mary', lang: 'English'}
+]
+
+createObjectCsvWriter(params).writeRecords(records)
